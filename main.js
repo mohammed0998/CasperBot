d618ee31edd4b7e19144539978afc57bb8263940
mohammed0998 <160079261+mohammed0998@users.noreply.github.com>
2024-02-26 00:31:20 +0100

    Update main.js

--- main.js
+++ main.js
@@ -490,7 +490,7 @@ setInterval(async () => {
   if (stopped === 'close' || !conn || !conn.user) return;
   const _uptime = process.uptime() * 1000;
   const uptime = clockString(_uptime);
-  const bio = `BOBIZA BOT [ ⏳ ] Uptime: ${uptime}`;
+  const bio = `𝙲𝙰𝚂𝙿𝙴𝚁 𝙱𝙾𝚃 [ ⏳ ] Uptime: ${uptime}`;
   await conn.updateProfileStatus(bio).catch((_) => _);
 }, 60000);
 function clockString(ms) {
